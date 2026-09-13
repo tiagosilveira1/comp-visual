654d3506c5a19fd69f6d084285ceaf6db6279dcc
Lukais <143547910+lukais01@users.noreply.github.com>
2026-09-13 18:05:43 +0000

    Adicionando o Histograma

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@ Vitor Alves Pereira, RA: 10410862
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <math.h>
 
 /*
 Constantes para a janela (1024x768)
@@ -44,6 +46,19 @@ struct MyWindow
   SDL_Window *window;
   SDL_Renderer *renderer;
   SDL_FRect rect;
+};
+
+//Estrutura histograma
+typedef struct Histograma Histograma;
+struct Histograma
+{
+  int valores[256];
+
+  double media;
+  double desvio_padrao;
+
+  const char *classificacao_brilho;
+  const char *classificacao_contraste;
 };
 
 //------------------------------------------------------------------------------
@@ -220,6 +235,233 @@ bool MyWindow_initialize_child(MyWindow *child, const char *title, int width, in
 }
 
 //------------------------------------------------------------------------------
+//Etapa 4: Gerar histograma da imagem
+/*
+Incrementa o histograma de acordo com a intensidade de cada pixel da imagem (já covertida para escala de cinza).
+*/
+
+Histograma *gerar_histograma(MyImage *image) {
+
+  Histograma *histograma = malloc(sizeof(Histograma));
+
+  if (!histograma)
+  {
+    SDL_Log("Erro ao alocar memoria para o histograma.");
+    return NULL;
+  }
+
+  for (int i = 0; i < 256; ++i)
+  {
+    histograma->valores[i] = 0;
+  }
+
+  int total_pixels = image->surface->w * image->surface->h;
+
+  /*
+  Inicializa o histograma com a frequência dos níveis de intensidade 
+  */
+  for (int row = 0; row < image->surface->h; ++row)
+  {
+    Uint32 *linha = (Uint32 *)((Uint8 *)image->surface->pixels + row * image->surface->pitch);
+
+    for (int col = 0; col < image->surface->w; ++col)
+    {
+      Uint32 pixel = linha[col];
+
+      Uint8 r, g, b;
+
+      SDL_GetRGB(pixel,SDL_GetPixelFormatDetails(image->surface->format),SDL_GetSurfacePalette(image->surface),&r,&g,&b);
+
+      histograma->valores[r]++;
+    }
+  }
+
+  /*
+  Calcular média em seguida
+  */
+  double soma = 0.0;
+
+  for (int intensidade = 0;intensidade < 256;++intensidade)
+  {
+    soma += intensidade * histograma->valores[intensidade];
+  }
+
+  histograma->media = soma / total_pixels;
+
+  /*
+  Classificar a imagem com base na média de brilho
+  */
+  if (histograma->media < 85.0)
+  {
+    histograma->classificacao_brilho = "escura";
+  }
+  else if (histograma->media < 170.0)
+  {
+    histograma->classificacao_brilho = "media";
+  }
+  else
+  {
+    histograma->classificacao_brilho = "clara";
+  }
+
+  /*
+  Calcular desvio padrão (sub etapa final)
+  */
+  double soma_quadrados = 0.0;
+
+  for (int intensidade = 0;intensidade < 256;++intensidade)
+  {
+    double diferenca =
+        intensidade - histograma->media;
+
+    soma_quadrados += (diferenca * diferenca) * histograma->valores[intensidade];
+  }
+
+  double variancia = soma_quadrados / total_pixels;
+
+  histograma->desvio_padrao = sqrt(variancia);
+
+  if (histograma->desvio_padrao < 50.0)
+  {
+    histograma->classificacao_contraste = "baixo";
+  }
+  else if (histograma->desvio_padrao < 100.0)
+  {
+    histograma->classificacao_contraste = "medio";
+  }
+  else
+  {
+    histograma->classificacao_contraste = "alto";
+  }
+
+  return histograma;
+}
+
+/*
+Desenha o histograma na janela secundária
+*/
+
+void desenhar_histograma(SDL_Renderer *renderer, Histograma *histograma)
+{
+  const float grafico_x = 10.0f;
+  const float grafico_y = 10.0f;
+  const float grafico_w = 300.0f;
+  const float grafico_h = 100.0f;
+
+  // Fundo branco
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderClear(renderer);
+
+  int maior_valor = 0;
+
+  // Encontrar o maior valor do histograma
+  for (int i = 0; i < 256; ++i)
+  {
+    if (histograma->valores[i] > maior_valor)
+    {
+      maior_valor = histograma->valores[i];
+    }
+  }
+
+  if (maior_valor == 0)
+  {
+    return;
+  }
+
+  // Cor preta para as barras
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+  float largura_barra = grafico_w / 256.0f;
+
+  for (int i = 0; i < 256; ++i)
+  {
+    if (histograma->valores[i] == 0)
+    {
+      continue;
+    }
+
+    float altura =
+        ((float)histograma->valores[i] /
+         (float)maior_valor) *
+        grafico_h;
+
+    SDL_FRect barra;
+
+    barra.x = grafico_x + i * largura_barra;
+    barra.y = grafico_y + grafico_h - altura;
+    barra.w = largura_barra + 1.0f;
+    barra.h = altura;
+
+    SDL_RenderFillRect(renderer, &barra);
+  }
+}
+
+/*
+Por fim, printa a média das intensidades e o desvio padrão na janela secundária, informando se é imagem clara, média ou escura
+*/
+
+void desenhar_informacoes(SDL_Renderer *renderer, TTF_Font *fonte, Histograma *histograma)
+{
+    SDL_Color preto = {0, 0, 0, 255};
+
+    char texto[128];
+
+    // -----------------------------------------
+    // Média de intensidade
+    // -----------------------------------------
+
+    snprintf(texto,sizeof(texto),"Media: %.2f - imagem %s",histograma->media,histograma->classificacao_brilho);
+
+    SDL_Surface *surface_texto = TTF_RenderText_Blended(fonte,texto,0,preto);
+
+    if (surface_texto)
+    {
+        SDL_Texture *texture_texto = SDL_CreateTextureFromSurface(renderer,surface_texto);
+        SDL_FRect rect;
+
+        rect.x = 10.0f;
+        rect.y = 115.0f;
+        rect.w = (float)surface_texto->w;
+        rect.h = (float)surface_texto->h;
+
+        SDL_DestroySurface(surface_texto);
+
+        if (texture_texto)
+        {
+            SDL_RenderTexture(renderer,texture_texto,NULL,&rect);
+            SDL_DestroyTexture(texture_texto);
+        }
+    }
+
+    // -----------------------------------------
+    // Desvio padrão
+    // -----------------------------------------
+
+    snprintf(texto,sizeof(texto),"Desvio padrao: %.2f - contraste %s",histograma->desvio_padrao,histograma->classificacao_contraste);
+
+    surface_texto = TTF_RenderText_Blended(fonte,texto,0,preto);
+
+    if (surface_texto)
+    {
+        SDL_Texture *texture_texto = SDL_CreateTextureFromSurface(renderer,surface_texto);
+        SDL_FRect rect;
+
+        rect.x = 10.0f;
+        rect.y = 135.0f;
+        rect.w = (float)surface_texto->w;
+        rect.h = (float)surface_texto->h;
+
+        SDL_DestroySurface(surface_texto);
+
+        if (texture_texto)
+        {
+            SDL_RenderTexture(renderer,texture_texto,NULL,&rect);
+            SDL_DestroyTexture(texture_texto);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
 /*Função utilizada do repositório do professor
 Disponível em: https://github.com/profkishimoto/CompVis262/blob/main/src/05-filter_image/main.c
 */
@@ -278,6 +520,56 @@ void MyImage_destroy(MyImage *image)
 
   SDL_Log("<<< MyImage_destroy()");
 }
+
+//------------------------------------------------------------------------------
+void loop(MyWindow *window, MyWindow *infoWindow, MyImage *image, Histograma *histograma, TTF_Font *fonte)
+{
+  SDL_Event event;
+
+  char windowTitle[WINDOW_TITLE_MAX_LENGTH] = {0};
+
+  bool isRunning = true;
+
+  while (isRunning)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_EVENT_QUIT:
+          isRunning = false;
+          break;
+
+        case SDL_EVENT_MOUSE_MOTION:
+
+          snprintf(windowTitle, WINDOW_TITLE_MAX_LENGTH, "%s (%.0f, %.0f)", "Hello, SDL_image", event.motion.x, event.motion.y);
+          SDL_SetWindowTitle(window->window, windowTitle);
+          break;
+      }
+    }
+
+    //---------------------------------------------
+    // Renderizar janela principal ----------------
+
+    SDL_RenderClear(window->renderer);
+
+    SDL_RenderTexture(window->renderer, image->texture, NULL, NULL);
+
+    SDL_RenderPresent(window->renderer);
+
+    //---------------------------------------------
+    // Renderizar janela secundária
+
+    SDL_RenderClear(infoWindow->renderer);
+
+    desenhar_histograma(infoWindow->renderer,histograma);
+
+    desenhar_informacoes(infoWindow->renderer,fonte,histograma);
+
+    SDL_RenderPresent(infoWindow->renderer);
+  }
+}
+
 //------------------------------------------------------------------------------
 void shutdown(void)
 {
@@ -317,7 +609,7 @@ int main(int argc, char *argv[])
   infoWindow.rect.h = 0.0f;
 
   //-----------------------------------------------
-  // Inicialização da SDL --------------------------
+  // Inicialização da SDL -------------------------
 
   if (!SDL_Init(SDL_INIT_VIDEO))
   {
@@ -326,6 +618,16 @@ int main(int argc, char *argv[])
   }
 
   atexit(shutdown);
+
+  //-----------------------------------------------
+  // Inicialização do ttf -------------------------
+
+  if (!TTF_Init())
+{
+    SDL_Log("Erro ao iniciar SDL_ttf: %s", SDL_GetError());
+    SDL_Quit();
+    return SDL_APP_FAILURE;
+}
 
   //-----------------------------------------------
   // Verificar argumento da imagem ----------------
@@ -341,7 +643,7 @@ int main(int argc, char *argv[])
   char *file = argv[1];
 
   //-----------------------------------------------
-  // Criar janela principal ------------------------
+  // Criar janela principal -----------------------
 
   if (!MyWindow_initialize(&window, WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT,0))
   {
@@ -352,7 +654,7 @@ int main(int argc, char *argv[])
   }
 
   //-----------------------------------------------
-  // Criar janela secundária -----------------------
+  // Criar janela secundária ----------------------
 
   if (!MyWindow_initialize_child(&infoWindow, INFO_WINDOW_TITLE, INFO_WINDOW_WIDTH, INFO_WINDOW_HEIGHT, window.window))
   {
@@ -364,7 +666,23 @@ int main(int argc, char *argv[])
   }
 
   //-----------------------------------------------
-  // Carregamento da imagem ------------------------
+  //Carregar fonte --------------------------------
+  TTF_Font *fonte = TTF_OpenFont("assets/DejaVuSans.ttf", 8);
+
+  if (!fonte)
+{
+    SDL_Log("Erro ao carregar fonte: %s", SDL_GetError());
+
+    MyWindow_destroy(&infoWindow);
+    MyWindow_destroy(&window);
+    TTF_Quit();
+    SDL_Quit();
+
+    return 1;
+}
+
+  //-----------------------------------------------
+  // Carregamento da imagem -----------------------
 
   MyImage *image = carregar_imagem(file, window.renderer);
 
@@ -373,6 +691,7 @@ int main(int argc, char *argv[])
     MyWindow_destroy(&window);
     MyWindow_destroy(&infoWindow);
 
+    TTF_Quit();
     SDL_Quit();
     return 1;
   }
@@ -383,49 +702,24 @@ int main(int argc, char *argv[])
   conversao_escala_de_cinza(image, window.renderer);
 
   //-----------------------------------------------
+  // Histograma da imagem -------------------
+  Histograma *histograma = gerar_histograma(image);
+
+  if (!histograma)
+  {
+  SDL_Log("\t*** Erro ao criar o histograma da imagem.");
+  MyImage_destroy(image);
+  MyWindow_destroy(&infoWindow);
+  MyWindow_destroy(&window);
+
+  TTF_Quit();
+  SDL_Quit();
+  return 1;
+}
+
   // Loop principal -------------------------------
 
-  SDL_Event event;
-
-  char windowTitle[WINDOW_TITLE_MAX_LENGTH] = {0};
-
-  bool isRunning = true;
-
-  while (isRunning)
-  {
-    while (SDL_PollEvent(&event))
-    {
-      switch (event.type)
-      {
-        case SDL_EVENT_QUIT:
-          isRunning = false;
-          break;
-
-        case SDL_EVENT_MOUSE_MOTION:
-
-          snprintf(windowTitle, WINDOW_TITLE_MAX_LENGTH, "%s (%.0f, %.0f)", WINDOW_TITLE, event.motion.x, event.motion.y);
-          SDL_SetWindowTitle(window.window, windowTitle);
-          break;
-      }
-    }
-
-    //---------------------------------------------
-    // Renderizar janela principal ----------------
-
-    SDL_RenderClear(window.renderer);
-
-    SDL_RenderTexture(window.renderer, image->texture, NULL, NULL);
-
-    SDL_RenderPresent(window.renderer);
-
-    //---------------------------------------------
-    // Renderizar janela secundária
-    //---------------------------------------------
-
-    SDL_RenderClear(infoWindow.renderer);
-
-    SDL_RenderPresent(infoWindow.renderer);
-  }
+  loop(&window, &infoWindow, image, histograma, fonte);
 
   //-----------------------------------------------
   // Finalização -----------------------------------
@@ -434,6 +728,12 @@ int main(int argc, char *argv[])
 
   MyWindow_destroy(&infoWindow);
   MyWindow_destroy(&window);
+
+  TTF_CloseFont(fonte);
+  TTF_Quit();
+
+  SDL_Log("\t*** Liberando memória ...");
+  free(histograma);
 
   return 0;
 }
